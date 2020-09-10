3dc6d26f7b9add3ff80e3104548c3727d0f6587a
Ethan <ethan.le@a2baustralia.com>
2020-09-10 15:48:39 +1000

    try beebottle SDK ok

---
+++ src/services/beebotteService.js
@@ -0,0 +1,35 @@
+//Include the Beebotte SDK for nodejs
+const bbt = require('beebotte');
+
+const channelName = 'test';
+const channelToken = 'token_fISEmz2Vadllxt8r'; // need to securely
+const resourceName = 'vehicle';
+
+//Replace API and secret keys by those of your account
+var transport = {
+  type: 'mqtt',
+  token: channelToken,
+};
+
+// var transport = {
+//   type: 'mqtt',
+//   apiKey: 'apiKey',
+//   secretKey: 'secretKey', // Secret Key is a secret shared between you and Beebotte.
+// };
+
+//Create a Stream connector
+const client = new bbt.Stream({transport: transport});
+
+//On successful connection
+client.on('connected', function () {
+  //subscribe to a channel/resource
+  client.subscribe(channelName, resourceName, function (message) {
+    console.log('client.subscribe..');
+    console.log(message);
+  });
+  // //On successful subscription
+  // .on('subscribed', function (sub) {
+  //   console.log('client.publish..');
+  //   client.publish(channelName, resourceName, 'Hello World');
+  // });
+});
